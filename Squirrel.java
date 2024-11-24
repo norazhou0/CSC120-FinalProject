06eb63f3600d62088d39d9769fe262ecc2f46708
Jiawei Yu <jyu06@smith.edu>
2024-11-24 17:29:39 -0500

    squirrel2

--- Squirrel.java
+++ Squirrel.java
@@ -6,7 +6,20 @@ public class Squirrel extends Character {
         super(name, canOnboard, locationRow, locationColumn);
     }
 
+    public int getNAcorn() {
+        return nAcorn;
+    }
+
+    public boolean getAlreadyOpen() {
+        return alreadyOpen;
+    }
+
     public static void main(String[] args) {
         Squirrel squirrelFamily = new Squirrel("squirrel family", false, 4, 7);
+        squirrelFamily.getNAcorn();
+        squirrelFamily.getAlreadyOpen();
+        // Call the methods using the instance and print the results
+        // System.out.println("Number of Acorns: " + squirrelFamily.getNAcorn());
+        // System.out.println("Already Open: " + squirrelFamily.getAlreadyOpen());
     }
 }
